12d27569d9a93df95dde4df3b4e29b9dcf33f4f5
Benjamin Soltoff <ben@bensoltoff.com>
2020-07-29 14:58:07 -0500

    Add starter files

---
+++ 01-ggmap.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+library(ggmap)
+
+# import data
+chi_311 <- read_csv("data/chi-311.csv")
+glimpse(chi_311)
+
+# Obtain map tiles using ggmap for the city of Chicago
+
+
+# Generate a scatterplot of complaints about potholes in streets
+
+
+# Generate a heatmap of complaints about potholes in streets.
+# Do you see any unusual patterns or clusterings?
+
+
+# Obtain map tiles for Hyde Park
+
+
+# Generate a scatterplot of requests to pick up dead animals in Hyde Park
+
+
---
+++ 02-sf-maps.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+library(tidycensus)
+
+# Obtain information on median household income in 2017 for Cook County, IL
+# at the tract-level using the ACS. To retrieve the geographic features for
+# each tract, set `geometry = TRUE` in your function.
+# 
+# HINT: median household income is called B19013_001 by the Census Bureau
+
+
+# Draw a choropleth using the median household income data.
+# Use a continuous color gradient to identify each tract's median household income.
+
+
+# Draw the same choropleth for Cook County, but convert median household income
+# into a discrete variable with 6 levels.
+
+
+# Draw the same choropleth for Cook County using the discrete variable,
+# but select an appropriate color palette using Color Brewer
+
+
+# Use the `viridis` color palette for the Cook County map drawn
+# using the continuous measure.
+
+
